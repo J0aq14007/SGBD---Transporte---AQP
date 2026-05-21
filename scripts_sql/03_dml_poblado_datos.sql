5e2964ae3510162dfb938528baa3012e7c38b672
JoaqAr <132250399+J0aq14007@users.noreply.github.com>
2026-05-21 09:49:23 -0500

    feat: Agregando inserts Tablas 1 y 2

---
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -0,0 +1,7 @@
+-- tabla 1: EMPRESA_TRANSPORTE
+INSERT INTO EMPRESA_TRANSPORTE (nombre, ruc, telefono, correo) VALUES
+('SantaClara', '12345678901', '987654321', 'aqpbus@SantaClara.com'),
+
+--tabla  2:TERMINAL
+INSERT INTO TERMINAL (nombre, direccion, distrito, capacidad_buses) VALUES
+('Terminal Norte', 'Av. Sol 123', 'Paucarpata', 40),
