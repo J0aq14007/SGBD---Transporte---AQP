ae2720e6150dd03dd29f6dd0a2a06595897e723c
AdrianoG <adriano.guetat@ucsp.edu.pe>
2026-05-21 09:54:14 -0500

    feat: poblado de datos - centro_control y pasajero

--- scripts_sql/01_ddl_estructura.sql
+++ scripts_sql/01_ddl_estructura.sql
@@ -84,7 +84,7 @@ CREATE TABLE pasajero (
         CONSTRAINT pk_psjr_id_psjr PRIMARY KEY,
     nombres VARCHAR(50) NOT NULL,
     correo VARCHAR(100) NOT NULL,
-    telefono VARCHAR(15) NOT NOT,
+    telefono VARCHAR(15) NOT NULL,
 );
 
 -- tabla conductor (Independiente)
---
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -0,0 +1,5 @@
+INSERT INTO centro_control (nombre , ubicacion, telefono, horario_operacion)
+VALUES ("Centro Cayma", "Av Ejercito 001", "977211322", "6:00 AM - 10:00 PM"),
+
+INSERT INTO pasajero (nombres, correo, telefono)
+VALUES ("Adriano Guetat", "correoreal@dominioreal.com","986123543"),
