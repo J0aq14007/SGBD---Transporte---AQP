9ae62b06078f3bc16e727a594fdd8e85d5157b60
J0aq14007 <joaquinarenasriveros@gmail.com>
2026-05-23 20:30:25 -0500

    refactor: ordenadas consultas de las tablas en archivo DML

--- scripts_sql/02_dml_poblado_datos.sql
+++ scripts_sql/02_dml_poblado_datos.sql
@@ -1,8 +1,3 @@
-INSERT INTO centro_control (nombre , ubicacion, telefono, horario_operacion)
-VALUES ("Centro Cayma", "Av Ejercito 001", "977211322", "6:00 AM - 10:00 PM"),
-
-INSERT INTO pasajero (nombres, correo, telefono)
-VALUES ("Adriano Guetat", "correoreal@dominioreal.com","986123543"),
 -- tabla 1: EMPRESA_TRANSPORTE
 INSERT INTO EMPRESA_TRANSPORTE (nombre, ruc, telefono, correo) VALUES
 ('SantaClara', '12345678901', '987654321', 'aqpbus@SantaClara.com'),
@@ -10,6 +5,14 @@ INSERT INTO EMPRESA_TRANSPORTE (nombre, ruc, telefono, correo) VALUES
 --tabla  2:TERMINAL
 INSERT INTO TERMINAL (nombre, direccion, distrito, capacidad_buses) VALUES
 ('Terminal Norte', 'Av. Sol 123', 'Paucarpata', 40),
+
+
+INSERT INTO centro_control (nombre , ubicacion, telefono, horario_operacion)
+VALUES ("Centro Cayma", "Av Ejercito 001", "977211322", "6:00 AM - 10:00 PM"),
+
+INSERT INTO pasajero (nombres, correo, telefono)
+VALUES ("Adriano Guetat", "correoreal@dominioreal.com","986123543"),
+
 
 -- tabla conductor
 INSERT INTO conductor(nombres, licencia, telefono, fecha_ingreso)
