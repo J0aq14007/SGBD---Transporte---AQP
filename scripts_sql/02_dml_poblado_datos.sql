940c02b03d245df7d657c94321083e62e5694d0a
J0aq14007 <joaquinarenasriveros@gmail.com>
2026-05-23 20:34:18 -0500

    fix:renombrando nombres de tablas empresa y terminal

--- scripts_sql/02_dml_poblado_datos.sql
+++ scripts_sql/02_dml_poblado_datos.sql
@@ -1,20 +1,22 @@
--- tabla 1: EMPRESA_TRANSPORTE
-INSERT INTO EMPRESA_TRANSPORTE (nombre, ruc, telefono, correo) VALUES
+
+-- tabla  EMPRESA_TRANSPORTE
+INSERT INTO empresa_transporte(nombre, ruc, telefono, correo) VALUES
 ('SantaClara', '12345678901', '987654321', 'aqpbus@SantaClara.com'),
 
---tabla  2:TERMINAL
-INSERT INTO TERMINAL (nombre, direccion, distrito, capacidad_buses) VALUES
+--tabla  TERMINAL
+INSERT INTO terminal(nombre, direccion, distrito, capacidad_buses) VALUES
 ('Terminal Norte', 'Av. Sol 123', 'Paucarpata', 40),
 
-
+--tabla centro_control
 INSERT INTO centro_control (nombre , ubicacion, telefono, horario_operacion)
 VALUES ("Centro Cayma", "Av Ejercito 001", "977211322", "6:00 AM - 10:00 PM"),
 
+--tabla pasajero
 INSERT INTO pasajero (nombres, correo, telefono)
 VALUES ("Adriano Guetat", "correoreal@dominioreal.com","986123543"),
 
 
--- tabla conductor
+--tabla conductor
 INSERT INTO conductor(nombres, licencia, telefono, fecha_ingreso)
 VALUES('Manuel Perez', 'Q32453286', '913653286', '2025-02-03');
 
