869caa791075c751c0760e9765b2aafbd5ccb52e
J0aq14007 <joaquinarenasriveros@gmail.com>
2026-05-23 20:43:13 -0500

    Agregando registros tabla centro control y pasajero

--- scripts_sql/02_dml_poblado_datos.sql
+++ scripts_sql/02_dml_poblado_datos.sql
@@ -11,20 +11,29 @@ INSERT INTO empresa_transporte(nombre, ruc, telefono, correo) VALUES
 --tabla  TERMINAL
 INSERT INTO terminal(nombre, direccion, distrito, capacidad_buses) VALUES
 ('Terminal Norte', 'Av. Sol 123', 'Paucarpata', 40),
-('Terminal Sur', 'Av. Kennedy 456', 'José Luis Bustamante', 55),
+('Terminal Sur', 'Av. Los Incas 456', 'José Luis Bustamante', 50),
 ('Terminal Centro', 'Av. La Marina 789', 'Cercado', 30),
-('Terminal Este', 'Av. Los Incas 321', 'Mariano Melgar', 25),
+('Terminal Este', 'Av. Lima 321', 'Mariano Melgar', 35),
 ('Terminal Oeste', 'Av. Libertad 654', 'Cayma', 50);
 
 
 --tabla centro_control
 INSERT INTO centro_control (nombre , ubicacion, telefono, horario_operacion)
-VALUES ("Centro Cayma", "Av Ejercito 001", "977211322", "6:00 AM - 10:00 PM"),
+VALUES ('Centro Cayma', 'Av Ejercito 001', '977211322', '6:00 AM - 10:00 PM'),
+('Centro Paucarpata', 'Av. Dolores 222', '988112233', '7:00 AM - 9:00 PM'),
+('Centro Cercado', 'Av. Goyeneche 333', '977445566', '24 horas'),
+('Centro Socabaya', 'Av. Socabaya 444', '966778899', '5:00 AM - 11:00 PM'),
+('Centro Yanahuara', 'Av. Yanahuara 555', '955667788', '8:00 AM - 8:00 PM');
+
 
 
 --tabla pasajero
 INSERT INTO pasajero (nombres, correo, telefono)
-VALUES ("Adriano Guetat", "correoreal@dominioreal.com","986123543"),
+VALUES ('Adriano Guetat', 'correoreal@dominioreal.com','986123543'),
+('Lucía Torres', 'lucia.torres@mail.com','987654321'),
+('Carlos Díaz', 'carlos.diaz@mail.com','912345678'),
+('María López', 'maria.lopez@mail.com','933221144'),
+('José Ramos', 'jose.ramos@mail.com','955667788');
 
 
 --tabla conductor
