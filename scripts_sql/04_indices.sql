5b0c6ba8549e9584b9c96c35bfa9a4492f3af127
J0aq14007 <joaquinarenasriveros@gmail.com>
2026-06-18 08:34:34 -0500

    feat: Ejecutando Explains

--- scripts_sql/04_indices.sql
+++ scripts_sql/04_indices.sql
@@ -1,3 +1,4 @@
+--ANtes: Seq Scan on pasajero
 EXPLAIN ANALYZE
 SELECT *
 FROM pasajero
@@ -5,13 +6,13 @@ WHERE dni = '12345678';
 
 CREATE INDEX idx_pasajero_dni
 ON pasajero(dni);
-
+--Despues: Index Scan using idx_pasajero_dni
 EXPLAIN ANALYZE
 SELECT *
 FROM pasajero
 WHERE dni = '12345678';
 
-
+--ANtes: Seq Scan on bus	
 EXPLAIN ANALYZE
 SELECT *
 FROM bus
@@ -19,12 +20,13 @@ WHERE placa = 'ABC-123';
 
 CREATE INDEX idx_bus_placa
 ON bus(placa);
-
+--Despues: Index Scan using idx_bus_placa
 EXPLAIN ANALYZE
 SELECT *
 FROM bus
 WHERE placa = 'ABC-123';
 
+----ANtes: Seq Scan on viaje
 
 EXPLAIN ANALYZE
 SELECT *
@@ -33,7 +35,7 @@ WHERE fecha_salida = '2026-06-01';
 
 CREATE INDEX idx_viaje_fecha_salida
 ON viaje(fecha_salida);
-
+--Despues: Index Scan using idx_viaje_fecha_salida
 EXPLAIN ANALYZE
 SELECT *
 FROM viaje
